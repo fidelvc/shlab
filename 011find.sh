ef10ba4b006395b8f1ebe49253a5a6fbe44d0b3a
Fidel Verduguez Claros <fverdugu@cbr14s3.42barcelona.com>
2026-03-05 18:29:09 +0100

    add imprimir numero de directorios y archivos

--- 011find.sh
+++ 011find.sh
@@ -2,5 +2,6 @@
 
 # Busca y cuenta todos los archivos del directorio y los almacena en una variable. 
 
-number_of_files=$(find . -maxdepth 1 -type f | wc -l)
-echo "Hay ${number_of_files} archivos"
+number_of_files=`find . -maxdepth 1 -type f | wc -l`
+number_of_directories=`find . -maxdepth 1 -type d | wc -l`
+echo "Hay ${number_of_files} archivos y ${number_of_directories} directorios"
