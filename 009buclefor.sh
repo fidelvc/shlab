6cf619033d81f0db115c58034bf104a28161438e
Fidel Verduguez Claros <fverdugu@cbr14s3.42barcelona.com>
2026-03-05 18:18:20 +0100

    demo bucle for

---
+++ 009buclefor.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Iterar sobre una lista. 
+
+for fruta in manzana pera naranja; do
+	echo "Fruta: $fruta"
+done
+
+# Iterate over a range of numbers
+
+for i in {1..5}; do
+	echo "Number: ${i}"
+done
